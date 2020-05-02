7f256b8e297139638dc08b33cb1210e1d500c23e
jinyeong <jinyp91@gmail.com>
2020-05-02 15:55:35 +0900

    edit entities

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //외부에서는 생성하지 못하고 jpa 내부에서 리플렉션 또는 프록시로 사용하기 위한 생성자
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
